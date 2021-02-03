afc5edbcc5b20d0dfa44dc03f20aefa3d5ae7426
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-02-03 08:59:36 -0300

    now clicking spawns defenders

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    private Vector2 GetSquareClicked()
+    {
+        var mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        var worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        return worldPos;
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject newDefender = Instantiate(defender, GetSquareClicked(), Quaternion.identity) as GameObject;
+    }
+
+}
