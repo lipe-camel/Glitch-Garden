9cde407a82b750b506bf7037ff0072cf02209569
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-02-09 23:33:30 -0300

    added a script that gains stars (did it all on my own!)

---
+++ Assets/Scripts/Provider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Provider : MonoBehaviour
+{
+    [SerializeField] float mintime, maxtime;
+    [SerializeField] int stars;
+    [SerializeField] GameObject particle;
+    [SerializeField] Vector3 particleOffset;
+    bool looping = true;
+
+    StarDisplay starDisplay;
+
+    void Start()
+    {
+        starDisplay = FindObjectOfType<StarDisplay>();
+        StartCoroutine(AddStars());
+    }
+
+    private IEnumerator AddStars()
+    {
+        do
+        {
+            yield return new WaitForSeconds(Random.Range(mintime, maxtime));
+            starDisplay.AddStars(stars);
+            PlayVFX();
+        }
+        while (looping);
+    }
+
+    private void PlayVFX()
+    {
+        if (!particle) { return; }
+        GameObject vfx = Instantiate(particle, transform.position + particleOffset, Quaternion.identity);
+        var duration = vfx.GetComponent<ParticleSystem>().main.startLifetime.constantMax;
+        Destroy(vfx, duration);
+    }
+
+}
