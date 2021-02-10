dfe1d798af53dfeaccfb7923636cba2c7db05bb5
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-02-10 10:00:31 -0300

    now shooters can reckonize their lane and if there is a attacker in the lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,45 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] Projectile projectile;
     [SerializeField] Vector3 projectilePosition;
-    public void Fire()
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerOnLane())
+        {
+            //change animation state to shooting
+        }
+        else
+        {
+            //change animation state to idle
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool isMyLane = (Mathf.RoundToInt(spawner.transform.position.y) - Mathf.RoundToInt(transform.position.y) == 0);
+
+            if (isMyLane)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerOnLane()
+    {
+        return myLaneSpawner.transform.childCount > 0;
+    }
+
+    public void Fire() //acessed my animator controller
     {
         Instantiate(projectile, transform.position + projectilePosition, Quaternion.identity);
     }
