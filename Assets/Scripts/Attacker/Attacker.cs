c57b7a71e74a0af8ec08987f34014da1c3bb407f
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-02-13 09:30:21 -0300

    Created a Fox Enemy

--- Assets/Scripts/Attacker/Attacker.cs
+++ Assets/Scripts/Attacker/Attacker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     float movementSpeed = 1f; //acessed by animation event
-    int damage = 20;
+    [SerializeField] int damage = 20;
     GameObject currentTarget;
     Animator animator;
 
---
+++ Assets/Scripts/Attacker/Fox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    Attacker attacker;
+    Animator animator;
+
+
+    private void Start()
+    {
+        attacker = GetComponent<Attacker>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("hasJumped", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        if (otherObject.GetComponent<Tank>() && animator.GetBool("hasJumped") == false)
+        {
+            animator.SetTrigger("Jump");
+            animator.SetBool("hasJumped", true);
+        }
+        else if (otherObject.GetComponent<Defender>())
+        {
+            attacker.Attack(otherObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        if (otherObject.GetComponent<Defender>())
+        {
+            Debug.Log(gameObject.name + " exited defender collision");
+            attacker.UpdateAnimationState();
+        }
+    }
+}
