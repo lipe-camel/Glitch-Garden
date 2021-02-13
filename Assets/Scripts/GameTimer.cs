6e5f6ad7f7f0cc3583c4ea32ce841a9de1a43e62
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-02-13 14:25:42 -0300

    added a way to read the time of a level

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level timer in SECONDS")]
+    [SerializeField] float levelTime = 10;
+
+    Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        slider.value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if (timerFinished)
+        {
+            Debug.Log("level timer expired!");
+        }
+    }
+}
